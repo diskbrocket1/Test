8c98dbf1670f19775906817efd9a4da75a5e5c3e
Anna Bergström <annabergstrom@Annas-MacBook-Pro-3.local>
2021-03-03 21:39:05 +0100

    Worked on VaR

--- Variance-Covariance VaR.py
+++ Variance-Covariance VaR.py
@@ -1,19 +1,57 @@
 import pandas as pd
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 import numpy as np
 from scipy.stats import kurtosis, skew
 import yfinance as yf
 from yahoofinancials import YahooFinancials
 
-tsla_df = yf.download(['TSLA','AAPL'],
+
+w = np.array([0.5, 0.5])
+
+df = yf.download(['TSLA','AAPL'],
                       start='2019-01-01',
                       end='2019-12-31',
                       progress=False)
 
-print(tsla_df.info())
-print(tsla_df.columns)
+
+df_close = df[[("Close","AAPL"),("Close","TSLA")]].dropna()
+
+df_close = df_close.to_numpy().T
+
+df_close = np.log(np.delete(df_close, -1, 1)) - np.log(np.delete(df_close, 0, 1))
+
+cov_mat = np.cov(df_close, bias=False)
+
+avg_rates =  np.mean(df_close, axis=1).dot(w.T)
+
+print(avg_rates)
+
+port_std = np.sqrt(w.dot(cov_mat).dot(w.T))*-1.645
+
+print(port_std)
 
 
-plt.plot(tsla_df.index.to_pydatetime(), tsla_df['Close'])
 
-plt.show()
+
+
+
+
+# print(np.cov(df_close, bias=False))
+# print(np.mean(df_close[0][:]))
+# print(np.mean(df_close[1][:]))
+
+
+
+
+
+
+
+
+
+
+
+
+
+# plt.plot(tsla_df.index.to_pydatetime(), tsla_df['Close'])
+#
+# plt.show()
